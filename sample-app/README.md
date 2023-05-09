a2e34623a0d9c4a9c3e80b3f707138ab3b465c00
Janaka Abeywardhana <contact@janaka.co.uk>
2023-05-09 18:19:01 +0100

    refactor(sample-app): add client code via cli

--- sample-app/README.md
+++ sample-app/README.md
@@ -1,3 +1,7 @@
 # Sample App
 
 Used for end2end testing the fabr-bind-cli.
+
+- `node ../../fabr-bind-cli/dist/index.js client-gen --language=typescript` Run the `client-gen` CLI in the `src` to add the generated secret binding class. Then reference it in `index.ts`.
+
+- `npm i && npm run build && node dist/index.js` Build and run the sample app.
--- sample-app/src/index.ts
+++ sample-app/src/index.ts
@@ -1,12 +1,12 @@
 import { FakeSecretService } from "./fabr-bind/libs/FakeSecretService";
 // import { IFabrParams } from "./fabr-bind/libs/IFabrParams";
-import { MyParams, MySecrets } from "./fabr-bind";
+import { MySecrets } from "./fabr-bind/MySecrets";
 
 //example of instantiating the generated class.
-const db1ConnectionString = new MySecrets(new FakeSecretService()).database1(); // returns the secret value from the secret store.
+const mysecrets = new MySecrets(new FakeSecretService()); // returns the secret value from the secret store.
 
-console.log(`Value from 'database1': ${db1ConnectionString}`)
+console.log(`Value from 'database1': ${mysecrets.database2()}`)
 
-const api1Endpoint = MyParams.api1;
+const api1Endpoint = mysecrets.api1();
 
 console.log(`Value from none secret param 'api1' ${api1Endpoint}`);
